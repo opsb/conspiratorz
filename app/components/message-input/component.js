783bb016c40dc38323d0923dcfbc265429a44e36
opsb <oliver@opsb.co.uk>
2015-11-03 22:59:07 +0100

    Clear message-input after hitting enter

--- app/components/message-input/component.js
+++ app/components/message-input/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     addMessage(text) {
+      this.set('text', null);
       this.sendAction('onAddMessage', text);
     }
   }
