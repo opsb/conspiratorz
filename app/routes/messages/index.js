26a95dc27c74de42dc061bc7919db14c38c3d9ed
opsb <oliver@opsb.co.uk>
2015-11-03 22:23:31 +0100

    Load message list from firebase

--- app/routes/messages/index.js
+++ app/routes/messages/index.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.find('message');
+  },
+
   actions: {
     addMessage(text) {
       const message = this.store.createRecord('message', {body: text});
